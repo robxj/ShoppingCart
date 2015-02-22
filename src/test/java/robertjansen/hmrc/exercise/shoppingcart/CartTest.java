75a5780d5f7733f63bc1035ddc1974bf446f1bbf
Rob J <robertxj@gmail.com>
2015-02-22 18:24:50 +0000

    Simplest possible for single apple

--- src/test/java/robertjansen/hmrc/exercise/shoppingcart/CartTest.java
+++ src/test/java/robertjansen/hmrc/exercise/shoppingcart/CartTest.java
@@ -1,14 +1,17 @@
 package robertjansen.hmrc.exercise.shoppingcart;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
 
 import org.junit.Test;
 
 public class CartTest {
 
 	@Test
-	public void test() {
-//		fail("Not yet implemented");
+	public void testCheckoutSingleApple() {
+		BigDecimal cost = new Cart(new String[]{"apple"}).checkout();
+		assertEquals("Wrong cost", BigDecimal.valueOf(60,2), cost);
 	}
 
 }
