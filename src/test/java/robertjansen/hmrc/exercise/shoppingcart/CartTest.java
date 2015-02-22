d8458022fb54a69153680401d6c9e917b40ae767
Rob J <robertxj@gmail.com>
2015-02-22 18:19:37 +0000

    Initial commit of project skeleton.

---
+++ src/test/java/robertjansen/hmrc/exercise/shoppingcart/CartTest.java
@@ -0,0 +1,14 @@
+package robertjansen.hmrc.exercise.shoppingcart;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CartTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+	}
+
+}
